6d59124787e6597e2da752f477fa4beed0049e7d
RodrigoLorensiMarques <rodrigolorensimarques@protonmail.com>
2026-08-16 00:43:41 -0300

    chore: atualiza gitignore

---
+++ src/config.py
@@ -0,0 +1,17 @@
+from pathlib import Path
+
+DIRETORIOS = [
+    Path(r"/Users/rodrigomarques/Documents/GitHub/files-converter/diretorio_teste"),
+    Path(r"/Users/rodrigomarques/Documents/GitHub/files-converter/outro_diretorio"),
+]
+
+# Conversão
+ESCALA = 1.0
+
+# Ritmo de varredura
+INTERVALO = 2
+
+
+# Logs
+LOG_ARQUIVO = Path("/Users/rodrigomarques/Documents/GitHub/files-converter/logs/pdf2gif.log")
+LOG_NIVEL = "INFO"
